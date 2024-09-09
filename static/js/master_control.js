bc8cd8fff9e83fbd46f1fd2166a3608378dc7c01
Tudolin <angellnadalin@gmail.com>
2024-09-09 14:12:54 -0300

    teste monstros

--- static/js/master_control.js
+++ static/js/master_control.js
@@ -448,31 +448,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
 
-    function addMonsterToDOM(monster) {
-        const boardCenter = document.querySelector('.board-center');
-        
-        if (boardCenter) {
-            const monsterElement = document.createElement('div');
-            monsterElement.classList.add('enemy-card');
-            monsterElement.dataset.monsterId = monster._id;
-            monsterElement.innerHTML = `
-                <h4>${monster.name}</h4>
-                <img src="${monster.img_url}" alt="${monster.name}" class="monster-image">
-                <div class="health-bar">
-                    <div class="health-fill" style="width: ${(monster.current_hp / monster.hp) * 100}%"></div>
-                    <div class="health-text">HP: ${monster.current_hp} / ${monster.hp}</div>
-                </div>
-                <p>${monster.resumo}</p>
-                <button class="remove-monster-button" data-monster-id="${monster._id}">Remover</button>
-            `;
-            boardCenter.appendChild(monsterElement);
-            updateMonstersList()
-        } else {
-            console.error('Elemento board-center não encontrado no DOM.');
-        }
-    }
-    
-
     document.getElementById('monster-list').addEventListener('click', function (e) {
         if (e.target.classList.contains('remove-monster-button')) {
             const monsterId = e.target.getAttribute('data-monster-id');
