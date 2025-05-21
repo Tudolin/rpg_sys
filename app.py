095f91c9c64596725568409036350d3e68557754
Rafael Nadalin <angellnadalin@gmail.com>
2025-05-21 15:49:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = Flask(__name__)
 
 app.secret_key = os.environ.get('SECRET_KEY') or 'a212d3b5e27f9cd2dfb8a9d18587ae51b2f88af9e1e95112'
 app.config['SESSION_PROTECTION'] = 'strong'
-app.config['SESSION_TYPE'] = ''
+app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 CORS(app)
